c71c09ab2ee732291b7dcf8ebfe42b354b0362e8
Amit Bar-Gil <amit217@yandex.com>
2022-12-06 21:59:17 +0200

    feat: first function cpp

--- project_2/main.cpp
+++ project_2/main.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 
+int addNumbers(int num1, int num2) { return num1 + num2; }
 
-int main(){
-    int firstNum {5};
-    int secondNum {5};
-    int sum = firstNum + secondNum;
-
-    std::cout << "This is the sum: " << sum << std::endl;
-
-    return 0;
+int main() {
+  int firstNum{5};
+  int secondNum{5};
+  int sum = addNumbers(firstNum, secondNum);
+  std::cout << "This is the sum: " << sum << std::endl;
+  return 0;
 }
