ce7e1b6e28fa65b304a84b8d201cd560f14ecb4e
Amit Bar-Gil <amit217@yandex.com>
2022-12-06 22:21:22 +0200

    feat: getting full line input

--- project_3/main.cpp
+++ project_3/main.cpp
@@ -7,15 +7,19 @@ int main() {
   //   std::cerr << "This is an error message" << std::endl;
   //   std::clog << "This is a log message" << std::endl;
 
-  int age;
-  std::string name;
+  //   int age;
+  //   std::string name;
 
-  std::cout << "Enter your name and age: " << std::endl;
-  std::cin >> name;
-  std::cin >> age;
+  //   std::cout << "Enter your name and age: " << std::endl;
+  //   std::cin >> name;
+  //   std::cin >> age;
+  //   std::cout << "Hello " << name << " You are " << age << " years old!"
+  //             << std::endl;
 
-  std::cout << "Hello " << name << " You are " << age << " years old!"
-            << std::endl;
+  std::string fullName;
+  std::cout << "Enter your full name now please" << std::endl;
+  std::getline(std::cin, fullName);
+  std::cout << "Your full name is " << fullName << std::endl;
 
   return 0;
 }
