4e7370448293d317348650c8fa4a977bc1207eba
Amit Bar-Gil <amit217@yandex.com>
2022-12-06 23:21:14 +0200

    test: get size of int

--- project_5/main.cpp
+++ project_5/main.cpp
@@ -10,5 +10,10 @@ int main() {
   std::cout << "Undefined " << undefined << std::endl; // Will print 0
   std::cout << "Four " << four << std::endl;
 
+  std::cout << "Size of int " << sizeof(int) << std::endl;
+  std::cout << "Size of garbage " << sizeof(garbage) << std::endl;
+  std::cout << "Size of undefined int " << sizeof(undefined) << std::endl;
+  std::cout << "Size of defined int " << sizeof(four) << std::endl;
+
   return 0;
 }
