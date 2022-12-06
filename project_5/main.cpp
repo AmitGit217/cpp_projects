af421b39391d43f5e6c4a6db92ec3a70ffc8e069
Amit Bar-Gil <amit217@yandex.com>
2022-12-06 23:15:27 +0200

    docs: comment

--- project_5/main.cpp
+++ project_5/main.cpp
@@ -7,7 +7,7 @@ int main() {
   int four{4};
 
   std::cout << "Garbage " << garbage << std::endl;
-  std::cout << "Undefined " << undefined << std::endl;
+  std::cout << "Undefined " << undefined << std::endl; // Will print 0
   std::cout << "Four " << four << std::endl;
 
   return 0;
