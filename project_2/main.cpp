da529fc0233a833869a51665d31c171ddacfdf12
Amit Bar-Gil <amit217@yandex.com>
2022-12-06 21:48:37 +0200

    feat: add to numbers and print to the console

---
+++ project_2/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+
+int main(){
+    int firstNum = 12;
+    int secondNum = 5;
+    int sum = firstNum + secondNum;
+
+    std::cout << "This is the sum: " << sum << std::endl;
+
+    return 0;
+}
