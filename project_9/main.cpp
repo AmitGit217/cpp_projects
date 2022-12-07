9d81e9c744dea6f7c10fcf4d571ac552b7190076
Amit Bar-Gil <amit217@yandex.com>
2022-12-07 23:48:10 +0200

    feat: char

---
+++ project_9/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+
+  char name = 'a';
+  char a = 65;
+
+  std::cout << name << std::endl;
+  std::cout << "This is a letter from ascii:  " << a << std::endl;
+
+  return 0;
+}
