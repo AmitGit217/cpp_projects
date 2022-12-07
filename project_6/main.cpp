a65b2cf042820d0c40db9daac1c0e1a818c8259d
Amit Bar-Gil <amit217@yandex.com>
2022-12-07 23:04:32 +0200

    docs: modifiers and memory managment

--- project_6/main.cpp
+++ project_6/main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 
 int main() {
-  unsigned int num1{4}; // Only positive
-  signed int num2{-4};  // Any int
+  signed int num1{-4};  // Used for any int value: Up to 4 bytes
+  unsigned int num2{4}; // Can only store non-negative values: Up to 4 bytes
+  short int num3{
+      2}; // Used for small integers Range: -32768 to 32767: Up to 2 bytes
+  long int num4{41231}; // -2147483647 to 2147483647
+  long long int num5{9999379036567657};
+  // 8 bytes max value is 9,223,372,036,854,775,807
 
-  std::cout << "This is the unsigned var " << num1 << std::endl;
-  std::cout << "This is the signed var " << num1 << std::endl;
+  // You can of course mix modifiers.
 
   return 0;
 }
