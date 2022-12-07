baaf9ba1658d649224d432edce06295aa6370b9a
Amit Bar-Gil <amit217@yandex.com>
2022-12-07 23:52:23 +0200

    feat: auto modifier

--- project_9/main.cpp
+++ project_9/main.cpp
@@ -4,9 +4,14 @@ int main() {
 
   char name = 'a';
   char a = 65;
-
   std::cout << name << std::endl;
   std::cout << "This is a letter from ascii:  " << a << std::endl;
+
+  auto str = 'a';
+  auto nu = 5;
+  auto flo = 5.3f;
+  auto dou = 4.5;
+  auto lo = 54.3l;
 
   return 0;
 }
