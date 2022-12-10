57d5322940ac118b9457fb70bee40b64b3b2f443
Amit Bar-Gil <amit217@yandex.com>
2022-12-10 16:20:53 +0200

    feat: manual table cmd

---
+++ project_12/main.cpp
@@ -0,0 +1,37 @@
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  string employee1 = "Amit";
+  int employee1Age = 22;
+
+  string employee2 = "John";
+  int employee2Age = 25;
+
+  string employee3 = "Bob";
+  int employee3Age = 43;
+
+  string employee4 = "Max";
+  int employee4Age = 32;
+
+  cout << "This table is using iomanip lib: " << endl;
+  cout << setw(5) << "Name";
+  cout << setw(10) << "Age" << endl;
+  cout << endl;
+  // The iomanip lib table
+  cout << setw(5) << employee1;
+  cout << setw(10) << employee1Age << endl;
+
+  cout << setw(5) << employee2;
+  cout << setw(10) << employee2Age << endl;
+
+  cout << setw(5) << employee3;
+  cout << setw(10) << employee3Age << endl;
+
+  cout << setw(5) << employee4;
+  cout << setw(10) << employee4Age << endl;
+
+  return 0;
+}
