1c89a93786ed4387ffcd91ba5d93aa5a4863a235
Amit Bar-Gil <amit217@yandex.com>
2022-12-10 16:57:13 +0200

    feat: some math things

---
+++ project_14/main.cpp
@@ -0,0 +1,27 @@
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  cout << "Absolute value of -5: " << abs(5) << endl;
+
+  cout << endl;
+
+  cout << "f(x) = e ^ x: " << exp(10) << endl;
+
+  cout << endl;
+
+  cout << "2 ^ 4 " << pow(2, 4) << endl;
+
+  cout << endl;
+
+  cout << "log_10 of 1000: " << log10(1000) << endl;
+
+  cout << endl;
+
+  cout << "Sqrt of 81: " << sqrt(81) << endl;
+
+  return 0;
+}
